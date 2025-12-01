57dff9725456fd38bb10cd79f031c05d527fddbc
GhassenHammami <mohamedghassen.hammami@polytechnicien.tn>
2025-12-01 19:00:22 +0100

    Remove drawer options menu

--- app/src/main/java/com/example/cyberguard/DrawerActivity.java
+++ app/src/main/java/com/example/cyberguard/DrawerActivity.java
@@ -44,13 +44,6 @@ public class DrawerActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
-        return true;
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_drawer);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
